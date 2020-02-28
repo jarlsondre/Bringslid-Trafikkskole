ca7b9eb2398bc300729abecf076d24724573f17f
jarlsondre <60541573+jarlsondre@users.noreply.github.com>
2020-02-28 18:14:32 +0100

    Add files via upload
    La til alle filene som hører til

---
+++ js-test.js
@@ -0,0 +1,16 @@
+
+document.getElementById("para1").innerHTML = '';
+
+var tall = 0;
+
+function minFunksjon() {
+	document.getElementById("para1").innerHTML = "wallah bror, helt ærlig ingenting ass";
+	document.getElementById("body").style.backgroundColor = "rgb(255, 255, 255)";
+	tall++;
+}
+
+function skiftSide() {
+		window.location.href = "jarl2.html";
+}
+
+
