aff125a488c371623f72c2c8b6d1d4f0bb2b046c
jarlsondre <60541573+jarlsondre@users.noreply.github.com>
2020-08-15 19:37:51 +0200

    changed from columns to rows

--- js-test.js
+++ js-test.js
@@ -1,15 +1,15 @@
 
 
 
-function resize() {
-		if ($(window).width() < 1000) {
-		document.getElementById("col1").className="row justify-content-center";
-		document.getElementById("col2").className="row justify-content-center";
-		}
-		else {
-		document.getElementById("col1").className="col-6";
-		document.getElementById("col2").className="col-6";
-		}
-}
+// function resize() {
+//		if ($(window).width() < 1000) {
+//		document.getElementById("col1").className="row justify-content-center";
+//		document.getElementById("col2").className="row justify-content-center";
+//		}
+//		else {
+//		document.getElementById("col1").className="col-6";
+//		document.getElementById("col2").className="col-6";
+//		}
+// }
 
 
